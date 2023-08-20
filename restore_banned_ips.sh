8af6929208252a75590eb36403e6ca832acf2b2f
mmdzov <mzov939@gmail.com>
2023-08-20 06:48:13 +0330

    Added restore_banned_ips bash

---
+++ restore_banned_ips.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CSV_FILE="blocked_ips.csv"
+
+function restore_banned_ips() {
+  while IFS=',' read -r ip end_time; do
+    current_time=$(date +%s)
+    if (( current_time >= end_time )); then
+      iptables -D INPUT -s $ip -j DROP
+    else
+      iptables -A INPUT -s $ip -j DROP
+    fi
+  done < "$CSV_FILE"
+}
+
+
+restore_banned_ips
